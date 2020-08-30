35e38a1e73897a6b958dcb202bf59490ce4c6d7d
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-08-30 20:30:54 +0100

    version 1.0
    Instead of only creating a new tab, now it opens the tab only if not already opened and in both cases it changes the current tab to the app's one.

--- background.js
+++ background.js
@@ -1,11 +1,49 @@
-function openPage() {
+url = "https://to-do.live.com/tasks/myday";
 
-    browser.tabs.create({
-        url: "https://to-do.microsoft.com/",
-        index: 0,
-        pinned: true,
-        active: false
-    });
+function startApp() {
+    // Get openned tabs and process them to open new one
+    let querying = browser.tabs.query({pinned: true, windowId: browser.windows.WINDOW_ID_CURRENT});
+    querying.then(openTab, onError);
+}
+  
+function openTab(tabs) {
+    // Check if tab is already created
+    appTab = undefined;
+    for (let tab of tabs) {
+        if (tab.url.indexOf(url)>=0) {
+            appTab = tab;
+        }
+    }
+
+    if (appTab==undefined) {
+        // Open it if not
+        browser.tabs.create({
+            url: url,
+            index: 0,
+            pinned: true,
+            active: false
+        }).then(changeTab);
+    } else {
+        // Otherwise, change to openned tab
+        changeTab(appTab);
+    }
 }
 
-browser.browserAction.onClicked.addListener(openPage);
+function changeTab(tab) {
+    // Changes current tab to the open given as argument
+    browser.tabs.update(tab.id, {
+        active: true
+    }).then(onUpdated, onError);
+}
+
+// Debugging functions
+function onUpdated(tab) {
+    console.log(`Updated tab: ${tab.id}`);
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+// Add listener
+browser.browserAction.onClicked.addListener(startApp);
